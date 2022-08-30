dba095b4679104f83fbacc5375ddd35ae8569ab2
Ilyasse OUADDENE <LiveZizoo.maag@gmail.com>
2022-08-30 22:35:04 +0100

    CopyFiles with 1 TestBox

--- CopyFiles/frmMain.Designer.cs
+++ CopyFiles/frmMain.Designer.cs
@@ -28,91 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtFile = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.btnCopy = new System.Windows.Forms.Button();
             this.txtFolder = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.linkBrowseFile = new System.Windows.Forms.LinkLabel();
-            this.linkBrowseFolder = new System.Windows.Forms.LinkLabel();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.linkBrowseFolder = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(204, 19);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Emplacement de fichier";
-            // 
-            // txtFile
-            // 
-            this.txtFile.Location = new System.Drawing.Point(29, 46);
-            this.txtFile.Name = "txtFile";
-            this.txtFile.Size = new System.Drawing.Size(441, 28);
-            this.txtFile.TabIndex = 1;
             // 
             // btnCopy
             // 
             this.btnCopy.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCopy.Location = new System.Drawing.Point(169, 187);
+            this.btnCopy.Location = new System.Drawing.Point(169, 117);
             this.btnCopy.Name = "btnCopy";
             this.btnCopy.Size = new System.Drawing.Size(244, 54);
             this.btnCopy.TabIndex = 5;
             this.btnCopy.Text = "&Copier";
             this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
             // 
             // txtFolder
             // 
-            this.txtFolder.Location = new System.Drawing.Point(29, 136);
+            this.txtFolder.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtFolder.Location = new System.Drawing.Point(29, 66);
             this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
             this.txtFolder.Size = new System.Drawing.Size(441, 28);
             this.txtFolder.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 114);
+            this.label2.Location = new System.Drawing.Point(29, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(212, 19);
+            this.label2.Size = new System.Drawing.Size(214, 19);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Emplacement de dossier";
+            this.label2.Text = "Choisir un emplacement";
             // 
-            // linkBrowseFile
+            // ofd
             // 
-            this.linkBrowseFile.AutoSize = true;
-            this.linkBrowseFile.Location = new System.Drawing.Point(485, 51);
-            this.linkBrowseFile.Name = "linkBrowseFile";
-            this.linkBrowseFile.Size = new System.Drawing.Size(71, 19);
-            this.linkBrowseFile.TabIndex = 6;
-            this.linkBrowseFile.TabStop = true;
-            this.linkBrowseFile.Text = "Choisir";
+            this.ofd.Filter = "Tous Fichiers|*.*";
+            this.ofd.Title = "Ouvrir des fichiers";
             // 
             // linkBrowseFolder
             // 
             this.linkBrowseFolder.AutoSize = true;
-            this.linkBrowseFolder.Location = new System.Drawing.Point(485, 141);
+            this.linkBrowseFolder.FlatAppearance.BorderSize = 0;
+            this.linkBrowseFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.linkBrowseFolder.Image = global::CopyFiles.Properties.Resources.icons8_folder_36px;
+            this.linkBrowseFolder.Location = new System.Drawing.Point(487, 58);
             this.linkBrowseFolder.Name = "linkBrowseFolder";
-            this.linkBrowseFolder.Size = new System.Drawing.Size(71, 19);
-            this.linkBrowseFolder.TabIndex = 7;
-            this.linkBrowseFolder.TabStop = true;
-            this.linkBrowseFolder.Text = "Choisir";
+            this.linkBrowseFolder.Size = new System.Drawing.Size(48, 44);
+            this.linkBrowseFolder.TabIndex = 8;
+            this.linkBrowseFolder.UseVisualStyleBackColor = true;
+            this.linkBrowseFolder.Click += new System.EventHandler(this.linkBrowseFolder_Click);
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 253);
+            this.ClientSize = new System.Drawing.Size(582, 201);
             this.Controls.Add(this.linkBrowseFolder);
-            this.Controls.Add(this.linkBrowseFile);
             this.Controls.Add(this.btnCopy);
             this.Controls.Add(this.txtFolder);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtFile);
-            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "frmMain";
@@ -124,14 +107,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtFile;
         private System.Windows.Forms.Button btnCopy;
         private System.Windows.Forms.TextBox txtFolder;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.LinkLabel linkBrowseFile;
-        private System.Windows.Forms.LinkLabel linkBrowseFolder;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.Button linkBrowseFolder;
     }
 }
 
--- CopyFiles/frmMain.cs
+++ CopyFiles/frmMain.cs
@@ -1,20 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace CopyFiles
 {
     public partial class frmMain : Form
     {
+        #region myCodes
+        private bool CopyFiles()
+        {
+            try
+            {
+                string extPath = Properties.Settings.Default.extPath.ToString();
+                string fileName = Path.GetFileName(extPath);
+                string nouvPath = txtFolder.Text;
+                string nouvFile = string.Format(@"{0}\{1}", nouvPath, fileName);
+
+                if (!Directory.Exists(nouvPath))
+                    Directory.CreateDirectory(nouvPath);
+
+                if (File.Exists(nouvFile))
+                    File.Delete(nouvFile);
+
+                File.Copy(extPath, nouvFile, true);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+        #endregion myCodes
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (CopyFiles())
+                MessageBox.Show("Votre fichier a copié correctement", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void linkBrowseFolder_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtFolder.Text = fbd.SelectedPath;
+            }
         }
     }
 }
